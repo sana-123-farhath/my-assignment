06c0337bf37a944707443b1dccc8c1b72a736d6c
sana-123-farhath <sanazoya47@gmail.com>
2025-10-06 00:13:57 +0530

    Day 1

---
+++ day1/Firefox.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double browserVersion = 100.2;
+String browserName = "Firefox";
+boolean Visible = true;
+int year = 1998;
+char Logo ='f';
+
+System.out.println(browserVersion);
+System.out.println(browserName);
+System.out.println(year);
+System.out.println(Logo);
+
+	
+		
+
+	}
+
+}
