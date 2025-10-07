2f60d4a41b90e58be59ea04e1a65186a0f9cdad6
sana-123-farhath <sanazoya47@gmail.com>
2025-10-07 23:11:01 +0530

    Add files via upload

---
+++ SeleniumSetup.java
@@ -0,0 +1,15 @@
+package week2.day2;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumSetup {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+			ChromeDriver driver=new ChromeDriver();
+			driver.get("http://leaftaps.com/opentaps/control/main");
+			}
+	}
+
+
